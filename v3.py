e28e68d0d4d304725876d4adf38721bf29e0be4b
Alex Kleider <alexkleider@protonmail.com>
2026-04-19 12:07:34 -0700

    Clean up v3.py code.

--- v3.py
+++ v3.py
@@ -3,43 +3,32 @@
 # File: v3.py
 
 """
+Primary purpose is to take a google formated contacts.csv file and
+output a "better4june.txt" file implemented as contacts2txt.py.
+
+Also provides json_dump.py and to_csv.py
 """
 import csv
 import json
 
 f_name = "forJune.data"
 f_json = "junes.json"
-contacts_file = "contacts.csv"
+contacts_file = "contacts.csv"  # must be imported or decrypted!!
+better4june = "better4june.txt"
 
-g_field_names = (   # keys used by google's csv contacts listing
-"First Name","Middle Name","Last Name","Phonetic First Name",
-"Phonetic Middle Name","Phonetic Last Name","Name Prefix",
-"Name Suffix","Nickname","File As","Organization Name",
-"Organization Title","Organization Department","Birthday",
-"Notes","Photo","Labels","E-mail 1 - Label","E-mail 1 - Value",
-"E-mail 2 - Label","E-mail 2 - Value","Phone 1 - Label",
-"Phone 1 - Value","Phone 2 - Label","Phone 2 - Value",
-"Address 1 - Label","Address 1 - Formatted","Address 1 - Street",
-"Address 1 - City","Address 1 - PO Box","Address 1 - Region",
-"Address 1 - Postal Code","Address 1 - Country",
-"Address 1 - Extended Address", )
 
 def google_keys():
     g1stline ="""
     First Name,Middle Name,Last Name,Phonetic First Name,Phonetic Middle Name,Phonetic Last Name,Name Prefix,Name Suffix,Nickname,File As,Organization Name,Organization Title,Organization Department,Birthday,Notes,Photo,Labels,E-mail 1 - Label,E-mail 1 - Value,E-mail 2 - Label,E-mail 2 - Value,Phone 1 - Label,Phone 1 - Value,Phone 2 - Label,Phone 2 - Value,Address 1 - Label,Address 1 - Formatted,Address 1 - Street,Address 1 - City,Address 1 - PO Box,Address 1 - Region,Address 1 - Postal Code,Address 1 - Country,Address 1 - Extended Address
     """
-    stage1 = g1stline.strip()
-    stage2 = stage1.split(',')
-#   _ = input(stage2)
-#   stage3 = [f'"{item}"' for item in stage2]
-#   stage4 = ','.join(stage3)
-    return stage2
+    return g1stline.strip().split(',')
     
-field_names = ("First Name",             # limited set of keys
+field_names = ("First Name",      # keys June wants
                "Last Name",
                "E-mail 1 - Value" )
 
-def to_json(j_data, f_json):
+
+def json_dump(j_data, f_json):
     """
     Moves <j_data> (asumed to be a json
     compliant text file) to a json file.
@@ -48,6 +37,7 @@ def to_json(j_data, f_json):
         json.dump(j_data, jf, indent="  ")
     print(f"json data sent to {f_json}")
 
+
 def to_csv(mappings, csv_file):
     """
     Appends data (a list of dicts)
@@ -55,27 +45,24 @@ def to_csv(mappings, csv_file):
     - json.load(s) string into json.
     - json.dump(s) json into string.
     """
-#   j_data = json.loads(mappings)
     with open(csv_file, 'a', newline='') as outf:
         writer = csv.DictWriter(outf,
-                        fieldnames=g_field_names,
+                        fieldnames=google_keys(),
                         lineterminator='\n')
         writer.writerows(mappings)
-        pass
-    print(f"data appended to {csv_file}")
-    pass
+    print(f"Data appended to {csv_file}")
+
 
 def contacts2text(gcsvf, output_file):
     """
     Input is a exported gmail contacts.csv file.
     <output_file> is a text file listing last, first
-    and email fields.
+    and email fields, with an optional header.
     """
     ret = []
     with open(gcsvf, 'r') as inf:
         reader = csv.DictReader(inf, fieldnames=google_keys())
         for mapping in reader:
-#           _ = input(mapping)
             new_dict = {}
             new_dict['first'] = mapping["First Name"]
             new_dict['last']= mapping["Last Name"]
@@ -85,7 +72,9 @@ def contacts2text(gcsvf, output_file):
     ret = ret[1:]
     ret.sort(key=lambda x: x['last'])
     with open(output_file, 'w') as outf:
-        print("""
+        yn = input("Include a header? (y/n): ")
+        if yn and yn[0] in "yY":
+            print("""
 
 
     June's listing of Summer Camp Tea party invitees
@@ -101,9 +90,7 @@ def contacts2text(gcsvf, output_file):
 
 
 if __name__ == "__main__":
-#   print(google_keys())
-    contacts2text("contacts.csv", "better4june.txt")
-#   j_data = get_junes_data(fname=f_name)
-#   to_json(j_data, f_json)
+    contacts2text("contacts.csv", better4june)
+#   json_dump(j_data, f_json)
 #   to_csv(j_data, contacts_file)
-    pass
+
